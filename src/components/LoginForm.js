8093d7c32c6fc98cc5510f33490e4aba1518745e
Luis Zabala <luis.zabala076@gmail.com>
2019-06-12 21:56:36 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,33 +1,73 @@
 import React from "react";
 
+
 class LoginForm extends React.Component {
+
+  // this function will save the state of username and password as the user inputs the changes. If the username and password are black then the form will not use the callback function.  The form will prevent the default action of the form.
+
   constructor() {
     super();
 
-    this.state = {};
-  }
+    this.state = {
+      username: '',
+      password: ''
+    };
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  };
+
+  handleSubmit = event => {
+    event.preventDefault()
+    if (!this.state.username || !this.state.password) 
+    return
+    this.props.handleLogin(this.state)
+  };
+
+
 
   render() {
     return (
-      <form>
+
+      <form onSubmit={this.handleSubmit}>
+
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input 
+            id="username" 
+            name="username" 
+            type="text"
+            onChange={this.handleChange}        
+            />
           </label>
         </div>
+
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input 
+            id="password" 
+            name="password" 
+            type="password"
+            onChange={this.handleChange} 
+            />
           </label>
         </div>
+
         <div>
-          <button type="submit">Log in</button>
+          <button 
+          type="submit">
+            Log in
+          </button>
         </div>
+
       </form>
     );
-  }
-}
+  };
+};
 
 export default LoginForm;
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,18 +1,29 @@
+
 import React from "react";
 
 class TwitterMessage extends React.Component {
+  // we want to create a javascript file that allows a user to submit a form with a maxChar limit of 140.  This limit is located in App.js.  <TwitterMessage /> maxChars={140}/>  This limit should be readable and update as the user updates the form i.e message = [event.target.value] :[this.state.message]
+
   constructor() {
     super();
-
-    this.state = {};
+    this.state = {
+      message: ''
+    };
   }
 
   render() {
     return (
-      <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+
+    <div>
+        <strong> Your message: </strong> 
+        <input 
+        type = "text"
+        onChange = { event => this.setState({message: event.target.value})}
+        value = { this.state.message } 
+        /> 
+        {this.props.maxChars - this.state.message.length}
       </div>
+
     );
   }
 }
